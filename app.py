a259f38a06a05d4199fe540bf2b2fd0d642aa5a1
AkhilLaxman <58255687+AkhilLaxman@users.noreply.github.com>
2020-06-17 17:32:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+#import pickle
+from joblib import load
+app = Flask(__name__)
+model = load('lgr.save')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    x_test = [[float(x) for x in request.form.values()]]
+    print(x_test)
+    prediction = model.predict(x_test)
+    print(prediction)
+    output=prediction[0]
+    if(output==2):
+        pred="Readmits in the hospital"
+    else:
+        pred="Does not readmit in the hospital"
+
+    
+    return render_template('index.html', prediction_text='Patient {}'.format(pred))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
